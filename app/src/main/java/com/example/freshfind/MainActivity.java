39b3a7c572e137b6670ceff5f2a84dbe81d1ee85
AHTepsilon <alehertoro725@gmail.com>
2021-12-05 12:21:10 -0500

    Implemented time for first screen instead of pressing screen

--- app/src/main/java/com/example/freshfind/MainActivity.java
+++ app/src/main/java/com/example/freshfind/MainActivity.java
@@ -4,25 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button overlay;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        overlay = findViewById(R.id.firstScreenInteracts);
+        handler = new Handler();
 
-        overlay.setOnClickListener((view) ->
-        {
-            switchScreen();
-        });
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                switchScreen();
+            }
+        }, 2000);
     }
     public void switchScreen()
     {
